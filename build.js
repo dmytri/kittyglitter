dc5155750ec0a0cf924d83aed3a725d7ab748229
Dmytri Kleiner <dk@trick.ca>
2016-12-17 22:52:22 +0100

    metalsmith stub cont

--- build.js
+++ build.js
@@ -1,24 +1,23 @@
 ;(function () {
-var Metalsmith = require('metalsmith')
-var layouts = require('metalsmith-layouts');
-var markdown = require('metalsmith-markdown');
-var contentful = require('contentful-metalsmith');
+const Metalsmith = require('metalsmith')
+const layouts = require('metalsmith-layouts');
+const markdown = require('metalsmith-markdown');
+const contentful = require('contentful-metalsmith');
 
 Metalsmith(__dirname)
   .source('src')
   .destination('build')
-  .use(markdown())
-  .use(layouts({
-    engine: 'mustache'
-  }))    
   .use(contentful({
     space_id : process.env.CONTENTFUL_SPACE_ID,
     access_token : process.env.CONTENTFUL_ACCESS_TOKEN
   }))
+  .use(layouts({
+    engine: 'mustache',
+  }))    
+  .use(markdown())
   .build(function(err) {
-    console.log(process.env.SPACE_ID)
-    console.log(process.env.ACCESS_TOKEN)
     if (err) throw err
-    else console.log("OK")
+    else console.log("DONE")
   })
 }())
+
